ee086d208ae6a3a899ccd3d109668fb24d2be24b
haymary <marikoreneva@gmail.com>
2017-03-30 02:06:53 +0300

    Compressor changed

--- Compressors/AutoencoderCompressor.py
+++ Compressors/AutoencoderCompressor.py
@@ -3,7 +3,11 @@ from SimpleAutoencoder import SimpleAutoencoder
 
 class AutoencoderCompressor(Compressor):
     def compress(self, image):
+        im = self.preprocess(image)
         auto = SimpleAutoencoder()
         auto.load_models(num = str(num_try))
+        return auto.encode(im)
 
-        return auto.encode(image)
+    def preprocess(self, image):
+        #TODO: do some
+        image = image / 255.
